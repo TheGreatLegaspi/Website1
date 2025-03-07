404cba438d4a281b6ed7d5225f3422f5288bf72b
TheGreatLegaspi <Ramueljustin@icloud.com>
2025-03-07 09:40:50 -0800

    some text

--- js/expanding.js
+++ js/expanding.js
@@ -1,13 +1,13 @@
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
-    panel.addEventListener('click', ()=> {
-        removeActiveClasses().add('active')
-        panel.classList
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
     })
 })
 
-function removeActiveClasses (){
+function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
