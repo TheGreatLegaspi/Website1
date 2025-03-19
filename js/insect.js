ad07ea0a3361350b8cef8b0b42523542623bec98
TheGreatLegaspi <Ramueljustin@icloud.com>
2025-03-19 16:51:09 +0000

    Wed, Mar 19, 2025, 9:51 AM -07:00

---
+++ js/insect.js
@@ -0,0 +1,7 @@
+const screens = document.querySelectorAll('.screen')
+const start_btn = document.getElementbyId('start-btn')
+const choose_insect0_btns = document.querySelectorAll('choose-insect-btn')
+const game_container = document.getElementById ('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const mes= document.getElementById('message')
