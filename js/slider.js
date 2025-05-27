14547a0aef8b135aa64f0cbb61af843c71f811cb
TheGreatLegaspi <Ramueljustin@icloud.com>
2025-05-27 17:08:52 +0000

    Tue, May 27, 2025, 10:08 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -13,6 +13,9 @@ downButton.addEventListener('click', () => changeSlide('down'))
 
 function changeSlide(direction){
     if (direction == 'up'){
+        if (currentSlide == slidesLength) {
+            currentSlide = 0
+        }
         currentSlide = currentSlide + 1
         leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
         rightSide.style.top = `${-(currentSlide - 1) * 100}%`
