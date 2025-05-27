44578e8448cad079f97eea8585b3bd9ac654f8d6
TheGreatLegaspi <Ramueljustin@icloud.com>
2025-05-27 16:46:35 +0000

    Tue, May 27, 2025, 9:46 AM -07:00

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,9 @@
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('down-button')
 const rightSide = document.querySelector('.right-side')
+const leftSide = document.querySelector('.left-side')
 const slidesLength = rightSide.querySelectorAll('div').length
+
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
